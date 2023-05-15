54c3016fff91a24ad8fd9c7b88c949e58c38c2c4
zzl <qq445395578@gmail.com>
2023-05-15 20:02:28 +0800

    mvc2

--- src/main/java/com/demo/code/animal/generate/Main.java
+++ src/main/java/com/demo/code/animal/generate/Main.java
@@ -6,11 +6,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
+//    public static void main(String[] args) {
+//        Dog dog = new Dog();
+//        dog.eat();
+//        dog.eat(new Food("food1", "type1"), "name");
+//        dog.eat(new Food("food2", "type2"));
+//    }
+
     public static void main(String[] args) {
-        Dog dog = new Dog();
-        dog.eat();
-        dog.eat(new Food("food1", "type1"), "name");
-        dog.eat(new Food("food2", "type2"));
+
+        String childDataPath = "/videoCode/44130200451320002011";
+        String videoCode = childDataPath.substring(childDataPath.lastIndexOf("/") + 1);
+        System.out.println(videoCode);
     }
 
 }
--- src/main/java/com/miniSpring/mvc/web/DispatcherServlet.java
+++ src/main/java/com/miniSpring/mvc/web/DispatcherServlet.java
@@ -15,12 +15,26 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  *
  * 根本性需要Servlet支持
  * 然后就是url路径和方法的映射需要存储
  * 然后再在业务层使用反射调用方法的形式执行方法
+ *
+ *
+ *
+ *
+ * SpringMVC的容器直接管理跟DispatcherServlet相关的Bean，也就是Controller，ViewResolver等，并且SpringMVC容器是在DispacherServlet的init方法里创建的。
+ * 而Spring容器管理其他的Bean比如Service和DAO。
+ * 并且SpringMVC容器是Spring容器的子容器，
+ * 所谓的父子关系意味着什么呢，就是你通过子容器去拿某个Bean时，子容器先在自己管理的Bean中去找这个Bean，如果找不到再到父容器中找。但是父容器不能到子容器中去找某个Bean。
+ *
+ *
  *
  */
 public class DispatcherServlet extends HttpServlet {
@@ -49,7 +63,7 @@ public class DispatcherServlet extends HttpServlet {
          * 这里对应上了web.xml中的配置
          *         <init-param>
          *             <param-name>contextConfigLocation</param-name>
-         *             <!--初始化配置文件地址，所有的配置参数都由这里引入-->
+         *             <!-- 初始化配置文件地址，所有的配置参数都由这里引入 -->
          *             <param-value>/WEB-INF/minisMVC-servlet.xml</param-value>
          *         </init-param>
          */
@@ -84,12 +98,20 @@ public class DispatcherServlet extends HttpServlet {
             Object obj = null;
             Class<?> clz = null;
             try {
+                /**
+                 * TODO 这里是否会出现就是不是ioc容器管理的类，那么注入这些能获取到吗
+                 *
+                 */
                 clz = Class.forName(controllerName);
                 controllerClasses.put(controllerName,clz);
             } catch (ClassNotFoundException e) {
                 throw new RuntimeException(e);
             }
             try {
+                /**
+                 * 没错，TODO  需要处理这里,需要拿到ioc容器里面的对象，因为里面的构造器和@Autowired都是已经注入完毕的
+                 *
+                 */
                 obj = clz.newInstance();
                 controllerObjs.put(controllerName,obj);
             } catch (InstantiationException e) {
@@ -112,6 +134,18 @@ public class DispatcherServlet extends HttpServlet {
             if(methods !=null){
                 for (Method method : methods) {
 
+                    /**
+                     * 这里是否是建ioc容器里面的service给拿出来的?
+                     * 这样子就是可以实现注入的
+                     * 这样子自动生成是否有问题
+                     *
+                     * 不对哦，之前学习的ioc容器拿到的值都是例如属性或者构造器里面的参数获取到数据的，跟方法好像还没有联动起来
+                     *
+                     * \
+                     * TODO 如何与ioc容器进行联动呢
+                     * 看一下源码可能会好一点。
+                     *
+                     */
                     boolean annotationPresent = method.isAnnotationPresent(RequestMapping.class);
                     if (annotationPresent){
                         String name = method.getName();
@@ -193,12 +227,16 @@ public class DispatcherServlet extends HttpServlet {
             return;
         }
 
-
         Method method = this.mappingMethods.get(servletPath);
         Object o = this.mappingObjs.get(servletPath);
 
         Object invoke = null;
         try {
+
+            /**
+             * 准确的来讲，应该是这里的来去获取ioc容器的对象的吧?
+             * //TODO 有点忘记怎么@Autowired注入的了
+             */
             invoke = method.invoke(o);
         } catch (IllegalAccessException e) {
             throw new RuntimeException(e);
